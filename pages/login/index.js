ae96bf8a1a6db1b5d3cd0b71b40541d547b6bc3b
= <=>
2021-06-04 15:50:56 +0700

    ui for login and register

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,58 +1,88 @@
 import { Container, Row, Col, Button } from "react-bootstrap";
+import Link from 'next/link'
+import {useState, useEffect} from 'react'
 import { Header } from "../../components";
+import style from '../../styles/auth.module.scss'
 
 const Login = () => {
+  const [visible, setVisible] = useState(false)
+
+  useEffect(()=>{
+    if(visible){
+      document.getElementById('input-password').type='text'
+    }else{
+      document.getElementById('input-password').type='password'
+    }
+  }, [visible]);
   return (
     <>
       <Header title="Login" />
       <Container fluid className="bg-blue-light bg-main">
         <Row className="align-content-center">
           <Col md={{ span: 4, offset: 4 }}>
-            <h1 className="my-5 text-center">Login</h1>
+            <h1 className="my-5 text-center kanit">Login</h1>
             <form>
               <div className="mb-3">
-                <label htmlFor="exampleInputEmail1" className="form-label">
+                <label htmlFor="exampleInputEmail1" className="form-label kanit">
                   Username or Email
                 </label>
                 <input
-                  type="email"
+                  type="text"
                   className="form-control border-radius-10 py-4"
                   id="exampleInputEmail1"
                   aria-describedby="emailHelp"
                 />
-                <div id="emailHelp" className="form-text">
-                  We'll never share your email with anyone else.
-                </div>
               </div>
-              <div className="mb-3">
-                <label htmlFor="exampleInputPassword1" className="form-label">
+
+              <div className="mb-1">
+                <label htmlFor="input-password" className="form-label kanit">
                   Password
                 </label>
-                <input
-                  type="password"
-                  className="form-control border-radius-10 py-4"
-                  id="exampleInputPassword1"
-                />
+                <div className={`input-group ${style.brnone}`}>
+                  <input
+                    type="password"
+                    className={`form-control border-radius-10 py-4 ${style.rnone}`}
+                    id="input-password"
+                  />
+                  <div className={`px-2 input-group-append ${style.toogle}`}>
+                    {(!visible)?(
+                      <img src='./icon/open-eyes-icon.svg' onClick={()=>setVisible(true)}/>
+                    ):(
+                      <img src='./icon/close-eyes-icon.svg' onClick={()=>setVisible(false)}/>
+                    )
+                    }
+                  </div>
+                </div>
               </div>
-              <div className="mb-3 form-check">
-                <input
-                  type="checkbox"
-                  className="form-check-input border-radius-10"
-                  id="exampleCheck1"
-                />
-                <label className="form-check-label" htmlFor="exampleCheck1">
-                  Check me out
-                </label>
+
+              <div className="d-flex flex-row-reverse bd-highlight mb-4">
+                <span className="p-2 font-weight-bold robot">Forgot Password ?</span>
               </div>
-              <div className="d-grid gap-2">
-                <Button variant='none' className="btn-blue-dark border-radius-10 w-100 my-3 shadow-sm kanit" size='lg'>
+
+              <div className="d-grid gap-2 mb-5">
+                <Button
+                  variant="none"
+                  className="btn-blue-dark border-radius-10 w-100 my-3 shadow-sm kanit"
+                  size="lg"
+                >
                   Login
                 </Button>
-								<Button variant='none' className="border-radius-10 bg-grey w-100 text-black shadow-sm kanit" size='lg'>
-									<img src='icon/google-icon.svg' class='mr-3'></img>
-                  <small>Login with google</small>
+                <Button
+                  variant="none"
+                  className="border-radius-10 bg-grey w-100 text-black shadow-sm kanit"
+                  size="lg">
+                  <img src="icon/google-icon.svg" class={`mr-3 ${style.icon}`}></img>
+                  <span>Login with google</span>
                 </Button>
               </div>
+
+              <div className='mt-5 text-center kanit'>
+                <span>New user ?</span>
+                <Link href='/register'>
+                  <span className='text-blue-dark ml-2 font-weight-bold'> Register</span>
+                </Link>
+              </div>
+
             </form>
           </Col>
         </Row>
---
+++ pages/register/index.js
@@ -0,0 +1,133 @@
+import { Container, Row, Col, Button } from "react-bootstrap";
+import { Header } from "../../components";
+import Link from "next/link";
+import style from '../../styles/auth.module.scss'
+import {useState,useEffect} from 'react'
+
+const Login = () => {
+  const [visiblePassword, setVisiblePassword] = useState(false)
+  const [visibleConfirm, setVisibleConfirm] = useState(false)
+
+  useEffect(()=>{
+    if(visiblePassword){
+      document.getElementById('input-password').type='text'
+    }else{
+      document.getElementById('input-password').type='password'
+    }
+
+    if(visibleConfirm){
+      document.getElementById('input-confirm-password').type='text'
+    }else{
+      document.getElementById('input-confirm-password').type='password'
+    }
+  }, [visiblePassword, visibleConfirm]);
+
+  return (
+    <>
+      <Header title="Register" />
+      <Container fluid className="bg-blue-light bg-main">
+        <Row className="align-content-center">
+          <Col md={{ span: 4, offset: 4 }} className='mb-5'>
+            <h1 className="my-5 text-center kanit">Register</h1>
+            <form>
+              <div className="mb-3">
+                <label htmlFor="input-password" className="form-label kanit">
+                  Username
+                </label>
+                <input
+                  type="text"
+                  className="form-control border-radius-10 py-4"
+                  id="input-username"
+                  aria-describedby="emailHelp"
+                />
+              </div>
+
+              <div className="mb-3">
+                <label htmlFor="input-email" className="form-label kanit">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  className="form-control border-radius-10 py-4"
+                  id="input-email"
+                  aria-describedby="emailHelp"
+                />
+              </div>
+
+              <div className="mb-1">
+                <label htmlFor="input-password" className="form-label kanit">
+                  Password
+                </label>
+                <div className='input-group'>
+                  <input
+                    type="password"
+                    className={`form-control border-radius-10 py-4 ${style.rnone}`}
+                    id="input-password"
+                  />
+                  <div className={`px-2 input-group-append ${style.toogle}`}>
+                    {(!visiblePassword)?(
+                      <img src='./icon/open-eyes-icon.svg' onClick={()=>setVisiblePassword(true)}/>
+                    ):(
+                      <img src='./icon/close-eyes-icon.svg' onClick={()=>setVisiblePassword(false)}/>
+                    )
+                    }
+                  </div>
+                </div>
+
+              </div>
+
+              <div className="mb-5">
+                <label htmlFor="input-confirm-password" className="form-label kanit">
+                  Confirm Password
+                </label>
+                <div className='input-group'>
+                  <input
+                    type="password"
+                    className={`form-control border-radius-10 py-4 ${style.rnone}`}
+                    id="input-confirm-password"
+                  />
+                  <div className={`px-2 input-group-append ${style.toogle}`}>
+                    {(!visibleConfirm)?(
+                      <img src='./icon/open-eyes-icon.svg' onClick={()=>setVisibleConfirm(true)}/>
+                    ):(
+                      <img src='./icon/close-eyes-icon.svg' onClick={()=>setVisibleConfirm(false)}/>
+                    )
+                    }
+                  </div>
+                </div>
+
+              </div>
+              <div className="d-grid gap-2">
+                <Button
+                  variant="none"
+                  className="btn-blue-dark border-radius-10 w-100 my-3 shadow-sm kanit"
+                  size="lg"
+                >
+                  Register
+                </Button>
+                <Button
+                  variant="none"
+                  className="border-radius-10 bg-grey w-100 text-black shadow-sm kanit"
+                  size="lg"
+                >
+                  <img src="icon/google-icon.svg" class={`mr-3 ${style.icon}`}></img>
+                  <small>Login with google</small>
+                </Button>
+              </div>
+
+              <div className='mt-5 text-center kanit'>
+                <span>Already have account?</span>
+                <Link href='/login'>
+                  <span className='text-blue-dark ml-2 font-weight-bold'> Login</span>
+                </Link>
+              </div>
+
+            </form>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+export default Login;
