66af49b8a9f15302a32a57e8c4cd55020d2c2ab7
rpetzandrian <rpetzandrian@gmail.com>
2021-06-05 10:31:17 +0700

    profile layout

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -4,10 +4,20 @@ import { Col, Row } from "react-bootstrap"
 const Profile = () => {
   return (
     <>
-      <div className='container-fluid'>
-        <Row className='g-0 p-3' style={{ height: '100vh' }}>
-          <Col xs={3} className='p-0'>
-            <Sidebar />
+      <div className='container-fluid bg-blue-light bg-main'>
+        <Row className='gx-3 p-2' style={{ height: '100vh' }}>
+          <Col md={5} lg={4} xl={3} className='p-0'>
+            <Sidebar activeTabs={1} />
+          </Col>
+          <Col>
+            <Row className='w-100 h-100 ms-2'>
+              <Col xs={12} className='bg-secondary w-100' style={{ height: '35%', borderTopLeftRadius: '30px', borderTopRightRadius: '30px' }}>
+                test
+              </Col>
+              <Col xs={12} className='bg-warning w-100' style={{ height: '65%', borderBottomLeftRadius: '30px', borderBottomRightRadius: '30px' }}>
+                test
+              </Col>
+            </Row>
           </Col>
         </Row>
       </div>
