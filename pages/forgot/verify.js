7f2f16b40887143d115e082f5138c133aa6700bd
= <=>
2021-06-05 08:57:00 +0700

    add use form for form auth

--- pages/forgot/verify.js
+++ pages/forgot/verify.js
@@ -1,82 +1,132 @@
 import { Container, Row, Col, Button, Image } from "react-bootstrap";
 import { Header } from "../../components";
-import Link from 'next/link'
+import Link from "next/link";
+import { useForm } from "react-hook-form";
 
 const CodeVerify = () => {
-    return (
-        <>
-            <Header title="Reset Password" />
-            <Container fluid className="bg-blue-light bg-main">
-                <Row>
-                    <Col md={6} xs={12}>
-                        <Row className="justify-content-center align-items-center bg-main">
-                            <Col md={6}>
-                                <Image className='logo' src="/images/face2.png" />
-                            </Col>
-                        </Row>
-                    </Col>
-                    <Col md={6} xs={12} className="bg-white kanit">
-                        <Row className="justify-content-center align-items-center bg-main text-center">
-                            <Col md={8}>
-                                <h2 className='fw-bolder'>Reset Password</h2>
-                                <h5 className='mt-5 fw-bolder'>Enter verification code we just sent to your email address</h5>
-                                <form>
-                                    <div>
-                                        <div className="d-flex text-center">
-                                            <input
-                                                type="number"
-                                                className='mt-2 text-center form-control border-top-0 border-left-0 border-right-0 border-dark shadow-none py-4 b-bottom'
-                                                id="InputCode"
-                                                onInput={(e) => {
-                                                    e.target.value = Math.max(0, parseInt(e.target.value)).toString().slice(0, 1)
-                                                }}
-                                                min={0}
-                                            />
-                                            <input
-                                                type="number"
-                                                className='input-groupmt-2 text-center form-control border-top-0 border-left-0 border-right-0 border-dark shadow-none py-4 b-bottom'
-                                                id="InputCode"
-                                                onInput={(e) => {
-                                                    e.target.value = Math.max(0, parseInt(e.target.value)).toString().slice(0, 1)
-                                                }}
-                                                min={0}
-                                            />
-                                            <input
-                                                type="number"
-                                                className='mt-2 text-center form-control border-top-0 border-left-0 border-right-0 border-dark shadow-none py-4 b-bottom'
-                                                id="InputCode"
-                                                onInput={(e) => {
-                                                    e.target.value = Math.max(0, parseInt(e.target.value)).toString().slice(0, 1)
-                                                }}
-                                                min={0}
-                                            />
-                                            <input
-                                                type="number"
-                                                className='mt-2 text-center form-control border-top-0 border-left-0 border-right-0 border-dark shadow-none py-4 b-bottom'
-                                                id="InputCode"
-                                                onInput={(e) => {
-                                                    e.target.value = Math.max(0, parseInt(e.target.value)).toString().slice(0, 1)
-                                                }}
-                                                min={0}
-                                            />
-                                        </div>
-                                        <p className='text-grey-dark mt-3 mb-5 fw-bolder'>Didn't receive a code?
-                                            <Link href="#">
-                                                <a className='text-blue-dark fw-bolder'> Resend</a>
-                                            </Link>
-                                        </p>
-                                        <Button variant='none' className="btn-blue-dark border-radius-10 w-100 my-3 shadow-sm kanit" size='lg'>
-                                            Verify
-                                        </Button>
-                                    </div>
-                                </form>
-                            </Col>
-                        </Row>
-                    </Col>
-                </Row>
-            </Container>
-        </>
-    );
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const handleChange = (e) =>{
+    var nextState = e.srcElement || e.target
+    let data = e.target.value
+    if(data.length == 1){
+      while(nextState = nextState.nextElementSibling){
+        if (nextState == null){
+            break
+        }
+        if (nextState.tagName.toLowerCase() === "input") {
+                nextState.focus();
+                break;
+        }
+      }
+    }
+  }
+  const processVerify = (data) =>{
+    console.log(data)
+  }
+  return (
+    <>
+      <Header title="Reset Password" />
+      <Container fluid className="bg-blue-light bg-main">
+        <Row>
+          <Col md={6} xs={12}>
+            <Row className="justify-content-center align-items-center bg-main">
+              <Col md={6}>
+                <Image className="logo" src="/images/face2.png" />
+              </Col>
+            </Row>
+          </Col>
+          <Col md={6} xs={12} className="bg-white kanit">
+            <Row className="justify-content-center align-items-center bg-main text-center">
+              <Col md={8}>
+                <h2 className="fw-bolder">Reset Password</h2>
+                <h5 className="mt-5 fw-bolder">
+                  Enter verification code we just sent to your email address
+                </h5>
+                <form onSubmit={handleSubmit(processVerify)}>
+                  <div>
+                    <div className="d-flex text-center">
+                      <input
+                        {...register('code1',{required:"Field can't be empty"})}
+                        type="number"
+                        className="mt-2 text-center form-control border-top-0 border-left-0 border-right-0 border-dark shadow-none py-4 b-bottom"
+                        id="InputCode"
+                        onInput={(e) => {
+                          e.target.value = Math.max(0, parseInt(e.target.value))
+                            .toString()
+                            .slice(0, 1);
+                        }}
+                        min={0}
+                        onChange={(e)=>handleChange(e)}
+                      />
+                      <input
+                        {...register('code2',{required:"Field can't be empty"})}
+                        type="number"
+                        className="mt-2 text-center form-control border-top-0 border-left-0 border-right-0 border-dark shadow-none py-4 b-bottom"
+                        id="InputCode"
+                        onInput={(e) => {
+                          e.target.value = Math.max(0, parseInt(e.target.value))
+                            .toString()
+                            .slice(0, 1);
+                        }}
+                        min={0}
+                        onChange={(e)=>handleChange(e)}
+                      />
+                      <input
+                        {...register('code3',{required:"Field can't be empty"})}
+                        type="number"
+                        className="mt-2 text-center form-control border-top-0 border-left-0 border-right-0 border-dark shadow-none py-4 b-bottom"
+                        id="InputCode"
+                        onInput={(e) => {
+                          e.target.value = Math.max(0, parseInt(e.target.value))
+                            .toString()
+                            .slice(0, 1);
+                        }}
+                        min={0}
+                        onChange={(e)=>handleChange(e)}
+                      />
+                      <input
+                        {...register('code4',{required:"Field can't be empty"})}
+                        type="number"
+                        className="mt-2 text-center form-control border-top-0 border-left-0 border-right-0 border-dark shadow-none py-4 b-bottom"
+                        id="InputCode"
+                        onInput={(e) => {
+                          e.target.value = Math.max(0, parseInt(e.target.value))
+                            .toString()
+                            .slice(0, 1);
+                        }}
+                        min={0}
+                        onChange={(e)=>handleChange(e)}
+                      />
+                    </div>
+                    <p className="text-grey-dark mt-3 mb-5 fw-bolder">
+                      Didn't receive a code?
+                      <Link href="#">
+                        <a className="text-blue-dark fw-bolder"> Resend</a>
+                      </Link>
+                    </p>
+                    <Button
+                      id = 'btn-submit' 
+                      variant="none"
+                      className="btn-blue-dark border-radius-10 w-100 my-3 shadow-sm kanit"
+                      size="lg"
+                      type='submit'
+                    >
+                      Verify
+                    </Button>
+                  </div>
+                </form>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 };
 
 export default CodeVerify;
--- pages/register/verify.js
+++ pages/register/verify.js
@@ -2,20 +2,28 @@ import { Container, Row, Col, Button } from "react-bootstrap";
 import Link from 'next/link'
 import { useState, useEffect } from 'react'
 import { Header } from "../../components";
+import {useForm} from 'react-hook-form'
 
 const Verify = () => {
-  const [inputData, setInputData] = useState({
-    code1: null,
-    code2: null,
-    code3: null,
-    code4: null
-  })
+  const { register, handleSubmit, formState: { errors } } = useForm()
   //process login
-  const processVerify = (e) => {
-    e.preventDefault()
-    console.log(Object.values(inputData).toString().replace(/,/g, ""))
+  const processVerify = (data) => {
+    console.log(Object.values(data).toString().replace(/,/g, ""))
   }
-
+  const handleChange = (e) =>{
+    var nextState = e.srcElement || e.target
+    let data = e.target.value
+    if(data.length == 1){
+      while(nextState = nextState.nextElementSibling){
+        if (nextState == null)
+                break;
+            if (nextState.tagName.toLowerCase() === "input") {
+                nextState.focus();
+                break;
+            }
+      }
+    }
+  }
 
   return (
     <>
@@ -24,44 +32,44 @@ const Verify = () => {
         <Row className="align-content-center py-5">
           <Col md={{ span: 4, offset: 4 }} className='my-5'>
             <h1 className="my-5 text-center kanit">Verify Code</h1>
-            <form onSubmit={(e) => processVerify(e)}>
+            <form onSubmit={handleSubmit(processVerify)}>
               <div className="mb-3">
                 <div className='input-group'>
                   <input
+                    {...register('code1', {required:"field can't be empty"})}
                     type="number"
-                    className='mt-2 text-center form-control border-top-0 border-left-0 border-right-0 border-dark shadow-none py-4 b-bottom bg-transparent'
+                    className={`mt-2 text-center form-control border-top-0 border-left-0 border-right-0 border-dark shadow-none py-4 b-bottom bg-transparent`}
                     onInput={(e) => {
                       e.target.value = Math.max(0, parseInt(e.target.value)).toString().slice(0, 1)
                     }}
-                    onChange={(e) => { setInputData({ ...inputData, code1: e.target.value }) }}
-                    required
+                    onChange={(e)=>handleChange(e)}
                   />
                   <input
+                    {...register('code2', {required:"field can't be empty"})}
                     type="number"
-                    className='mt-2 text-center form-control border-top-0 border-left-0 border-right-0 border-dark shadow-none py-4 b-bottom bg-transparent'
+                    className={`mt-2 text-center form-control border-top-0 border-left-0 border-right-0 border-dark shadow-none py-4 b-bottom bg-transparent`}
                     onInput={(e) => {
                       e.target.value = Math.max(0, parseInt(e.target.value)).toString().slice(0, 1)
                     }}
-                    onChange={(e) => { setInputData({ ...inputData, code2: e.target.value }) }}
-                    required
+                    onChange={(e)=>handleChange(e)}
                   />
                   <input
+                    {...register("code3", {required:"field can't be empty"})}
                     type="number"
-                    className='mt-2 text-center form-control border-top-0 border-left-0 border-right-0 border-dark shadow-none py-4 b-bottom bg-transparent'
+                    className={`mt-2 text-center form-control border-top-0 border-left-0 border-right-0 border-dark shadow-none py-4 b-bottom bg-transparent`}
                     onInput={(e) => {
                       e.target.value = Math.max(0, parseInt(e.target.value)).toString().slice(0, 1)
                     }}
-                    onChange={(e) => { setInputData({ ...inputData, code3: e.target.value }) }}
-                    required
+                    onChange={(e)=>handleChange(e)}
                   />
                   <input
+                    {...register("code4", {required:"field can't be empty"})}
                     type="number"
-                    className='mt-2 text-center form-control border-top-0 border-left-0 border-right-0 border-dark shadow-none py-4 b-bottom bg-transparent'
+                    className={`mt-2 text-center form-control border-top-0 border-left-0 border-right-0 border-dark shadow-none py-4 b-bottom bg-transparent`}
                     onInput={(e) => {
                       e.target.value = Math.max(0, parseInt(e.target.value)).toString().slice(0, 1)
                     }}
-                    onChange={(e) => { setInputData({ ...inputData, code4: e.target.value }) }}
-                    required
+                    onChange={(e)=>handleChange(e)}
                   />
                 </div>
               </div>
