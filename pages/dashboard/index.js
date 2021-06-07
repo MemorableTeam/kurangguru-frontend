f529eff1052aaccb37b50a15497fa4420d83363a
deaedria <dea.edria@gmail.com>
2021-06-07 17:55:54 +0700

    updated ui for dashboard

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,25 +1,57 @@
 import Sidebar from "../../components/sidebar"
-import { Col, Row, Carousel, Image } from "react-bootstrap"
+import { Col, Row, Carousel, Image, Button, Form } from "react-bootstrap"
 import { Header } from "../../components";
 import { useState, useEffect } from 'react'
 import { useForm } from "react-hook-form";
 import Link from "next/link";
+import moment from 'moment'
 
 const UserDashboard = () => {
   const [index, setIndex] = useState(0);
+  const [value, setValue] = useState(null);
+  const [activeTabs, setActiveTabs] = useState(1);
 
   const handleSelect = (selectedIndex, e) => {
     setIndex(selectedIndex);
   };
 
-  // const today = moment();
-  // const from_date = today.startOf('week');
-  // const to_date = today.endOf('week');
-  // console.log({
-  //   from_date: from_date.toString(),
-  //   today: moment().toString(),
-  //   to_date: to_date.toString(),
-  // });
+  const today = moment().format("D MMMM gggg");
+  const today1 = moment().format("YYYY-MM-DD");
+  const format = moment(value).format("D MMMM gggg");
+  const pickDay = moment(value).format("DD");
+  const startOfWeek = moment(value ? value : setValue(today1)).startOf('isoWeek');
+  const endOfWeek = moment(value ? value : setValue(today1)).endOf('isoWeek');
+
+  var days = [];
+  var day = startOfWeek;
+
+  while (day <= endOfWeek) {
+    days.push(day.toDate());
+    day = day.clone().add(1, 'd');
+  }
+
+  var week = days.map(function (dayOfWeek, i) {
+    return moment(dayOfWeek).format("dd")
+  });
+
+  var date = days.map(function (dayOfWeek, i) {
+    return moment(dayOfWeek).format("DD")
+  });
+
+  var fullDate = days.map(function (dayOfWeek, i) {
+    return moment(dayOfWeek).format("YYYY-MM-DD")
+  });
+
+  // const data = [
+  //   {
+  //     start_at: "08.00",
+  //     end_at: "09.40",
+  //   },
+  //   {
+  //     start_at: "08.10",
+  //     end_at: "09.40",
+  //   }
+  // ]
 
   return (
     <>
@@ -63,18 +95,96 @@ const UserDashboard = () => {
                       </Carousel.Item>
                     </Carousel>
                   </Col>
+
                   <Col xs={12} className='mt-3 w-100' style={{ height: '52%', borderTopLeftRadius: '30px', borderTopRightRadius: '30px' }}>
                     <h5 className="py-3 bg-white px-4 mb-0 fw-500" style={{ borderTopLeftRadius: '30px', borderTopRightRadius: '30px' }}>
-                      Today, October 16
+                      {value ? format : today}
+                      <input type="date" className="float-end" id="dateInput" name="dateInput" onChange={(e) => setValue(e.target.value)}></input>
                     </h5>
-                    <div className="d-flex bg-white">
-                      {/* <Button className="d-flex align-items-center justify-content-between shadow-none bg-transparent border-0 border-bottom w-100 h-btn" onClick={handleShowPhone}>
-                        <div className="d-flex align-items-center w-max">
-                          <Image className="py-1" src="/icon/phone-icon.svg" style={{ height: '10%', width: '10%' }} />
-                          <p className="ms-3 pt-3 pb-0 fw-700 text-black">Phone Numbers</p>
-                        </div>
-                        <Image className="py-1" src="/icon/right-icon.svg" style={{ height: '60%', width: '10%' }} />
-                      </Button> */}
+                    <div className="d-flex bg-white d-flex align-items-center justify-content-center">
+                      <Button className={`${pickDay == date[0] ? 'btn-blue-dark' : 'bg-white text-black'} b-hover border-radius-10 shadow-none border-0 me-4`} onClick={(e) => setValue(fullDate[0])}>
+                        <p className="fw-600">{week[0]} </p>
+                        <p className="fw-600">{date[0]} </p>
+                      </Button>
+                      <Button className={`${pickDay == date[1] ? 'btn-blue-dark' : 'bg-white text-black'} b-hover border-radius-10 shadow-none border-0 me-4`} onClick={(e) => setValue(fullDate[1])}>
+                        <p className="fw-600">{week[1]} </p>
+                        <p className="fw-600">{date[1]} </p>
+                      </Button>
+                      <Button className={`${pickDay == date[2] ? 'btn-blue-dark' : 'bg-white text-black'} b-hover border-radius-10 shadow-none border-0 me-4`} onClick={(e) => setValue(fullDate[2])}>
+                        <p className="fw-600">{week[2]} </p>
+                        <p className="fw-600">{date[2]} </p>
+                      </Button>
+                      <Button className={`${pickDay == date[3] ? 'btn-blue-dark' : 'bg-white text-black'} b-hover border-radius-10 shadow-none border-0 me-4`} onClick={(e) => setValue(fullDate[3])}>
+                        <p className="fw-600">{week[3]} </p>
+                        <p className="fw-600">{date[3]} </p>
+                      </Button>
+                      <Button className={`${pickDay == date[4] ? 'btn-blue-dark' : 'bg-white text-black'} b-hover border-radius-10 shadow-none border-0 me-4`} onClick={(e) => setValue(fullDate[4])}>
+                        <p className="fw-600">{week[4]} </p>
+                        <p className="fw-600">{date[4]} </p>
+                      </Button>
+                      <Button className={`${pickDay == date[5] ? 'btn-blue-dark' : 'bg-white text-black'} b-hover border-radius-10 shadow-none border-0 me-4`} onClick={(e) => setValue(fullDate[5])}>
+                        <p className="fw-600">{week[5]} </p>
+                        <p className="fw-600">{date[5]} </p>
+                      </Button>
+                      <Button className={`${pickDay == date[6] ? 'btn-blue-dark' : 'bg-white text-black'} b-hover border-radius-10 shadow-none border-0 me-3`} onClick={(e) => setValue(fullDate[6])}>
+                        <p className="fw-600">{week[6]} </p>
+                        <p className="fw-600">{date[6]} </p>
+                      </Button>
+                    </div>
+
+                    <div className='d-flex justify-content-center bg-white d-tab'>
+                      <div className={`mx-3 py-1 tab ${activeTabs === 1 ? 'text-blue-dark' : 'text-grey-dark'}`} onClick={() => setActiveTabs(1)} >All schedule</div>
+                      <div className={`mx-3 py-1 tab ${activeTabs === 2 ? 'text-blue-dark' : 'text-grey-dark'}`} onClick={() => setActiveTabs(2)} >For you</div>
+                    </div>
+
+                    <div className={`py-2 tab ${activeTabs === 1 ? 'd-block bg-white' : 'd-none'}`}>
+                      <Row className="mx-3 px-2 py-2 t-schedule">
+                        <Col xs={3} className="text-center align-self-center">
+                          test
+                        </Col>
+                        <Col xs={9} className="mb-1">
+                          <Row className="t-schedule" className="mb-1">
+                            <Col xs={9} className="bg-grey">Test1</Col>
+                            <Col xs={3} className="bg-blue-dark">Test2</Col>
+                          </Row>
+                          <Row className="t-schedule" className="mb-1">
+                            <Col xs={9} className="bg-grey">Test1</Col>
+                            <Col xs={3} className="bg-blue-dark">Test2</Col>
+                          </Row>
+                        </Col>
+                      </Row>
+                    </div>
+
+
+                    <div className={`py-2 tab ${activeTabs === 2 ? 'bg-white d-block' : 'd-none'}`}>
+                      <Row className="mx-3 px-2 py-2 t-schedule">
+                        <Col xs={3} className="text-center align-self-center">
+                          test
+                        </Col>
+                        <Col xs={9} className="mb-1">
+                          <Row className="t-schedule" className="mb-1">
+                            <Col xs={9} className="bg-grey">Test1</Col>
+                            <Col xs={3} className="bg-blue-dark">Test2</Col>
+                          </Row>
+                          <Row className="t-schedule" className="mb-1">
+                            <Col xs={9} className="bg-grey">Test1</Col>
+                            <Col xs={3} className="bg-blue-dark">Test2</Col>
+                          </Row>
+                        </Col>
+                        <Col xs={3} className="text-center align-self-center">
+                          test
+                        </Col>
+                        <Col xs={9} className="mb-1">
+                          <Row className="t-schedule" className="mb-1">
+                            <Col xs={9} className="bg-grey">Test1</Col>
+                            <Col xs={3} className="bg-blue-dark">Test2</Col>
+                          </Row>
+                          <Row className="t-schedule" className="mb-1">
+                            <Col xs={9} className="bg-grey">Test1</Col>
+                            <Col xs={3} className="bg-blue-dark">Test2</Col>
+                          </Row>
+                        </Col>
+                      </Row>
                     </div>
                   </Col>
                 </Row>
