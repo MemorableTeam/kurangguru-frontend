3391f8ee3b0a57fae30e98af6a850b1befd27d66
rpetzandrian <rpetzandrian@gmail.com>
2021-06-08 14:05:57 +0700

    Detail class user & fasilitator UI

--- pages/fasilitator/class/[id].js
+++ pages/fasilitator/class/[id].js
@@ -1,7 +1,7 @@
 import { useRouter } from "next/router"
 import { Card, Col, Row } from "react-bootstrap"
-import Header from '../../components/header'
-import Sidebar from '../../components/sidebar'
+import Header from '../../../components/header'
+import Sidebar from '../../../components/sidebar'
 
 const data = {
   id: 1,
@@ -16,25 +16,21 @@ const topics = [
   {
     id: 1,
     topic_name: 'Apa itu Javascript',
-    score: 86,
     is_finished: true
   },
   {
     id: 2,
     topic_name: 'Sejarah Javascript',
-    score: 90,
     is_finished: true
   },
   {
     id: 3,
     topic_name: 'Sintaks Javascript',
-    score: null,
     is_finished: false
   },
   {
     id: 4,
     topic_name: 'Type data Javascript',
-    score: null,
     is_finished: false
   }
 ]
@@ -66,14 +62,6 @@ const classDetail = () => {
   const router = useRouter()
   const { id } = router.query
 
-  const setColor = (score) => {
-    if (score >= 90 && score <= 100) return 'text-success'
-    if (score < 90) return 'text-primary'
-    if (score < 80) return 'text-warning'
-    if (score < 40 && score >= 0) return 'text-danger'
-    return ''
-  }
-
   return (
     <>
       <Header title="Class Detail" />
@@ -82,7 +70,7 @@ const classDetail = () => {
           <Col md={5} lg={4} xl={3} className='p-0'>
             <Row>
               <Col md={12} className="h-activity">
-                <Sidebar activeTabs={3} rootDir={{ icon: '../icon', img: '../images' }} />
+                <Sidebar activeTabs={3} rootDir={{ icon: '../../icon', img: '../../images' }} />
               </Col>
               <Col></Col>
             </Row>
@@ -90,7 +78,7 @@ const classDetail = () => {
           <Col md={5} lg={5} xl={6}>
             <div className='ms-2 mt-3'>
               <h4 className=''>
-                <img src='../icon/back-icon.svg' className='p-0 me-2' />
+                <img src='../../icon/back-icon.svg' className='p-0 me-2' />
                 {data?.name}
               </h4>
             </div>
@@ -119,7 +107,7 @@ const classDetail = () => {
                     <div className='w-100 d-flex justify-content-start'>
                       <input type="checkbox" className="px-2 text-center mt-1 ms-2" value={item?.id} />
                       <p className='w-100 mx-3'>{item?.topic_name}</p>
-                      <p className={`${setColor(item?.score)} fw-bold text-center w-25`}>{item?.is_finished ? item?.score : (<label className='badge bg-danger rounded-pill'>Unfinished</label>)}</p>
+                      <p className={`fw-bold text-center w-25`}>{item?.is_finished ? (<label className='badge bg-success rounded-pill'>Finished</label>) : (<label className='badge bg-danger rounded-pill'>Unfinished</label>)}</p>
                     </div>
                   </>)
                 })}
@@ -129,7 +117,7 @@ const classDetail = () => {
           <Col md={2} lg={3} xl={3}>
             <div className='ms-2 mt-3 d-block'>
               <h4 className=''>
-                <img src='../icon/back-icon.svg' className='p-0 me-2 invisible' />
+                <img src='../../icon/back-icon.svg' className='p-0 me-2 invisible' />
               </h4>
             </div>
             <Card className='border-radius-10 p-3 mt-3'>
@@ -137,16 +125,16 @@ const classDetail = () => {
               <div className='w-100 d-flex justify-content-start'>
                 <input type="checkbox" className="px-2 text-center mt-1 ms-2" value='' />
                 <p className='w-100 mx-3'>Select all</p>
-                <p className='fw-bold text-center'><img src='../icon/list-icon.svg' /></p>
+                <p className='fw-bold text-center'><img src='../../icon/list-icon.svg' /></p>
               </div>
               <hr />
               {member && member?.map(item => {
                 return (<>
                   <div className='w-100 d-flex justify-content-start align-items-center'>
                     <input type="checkbox" className="px-2 text-center mt-1 ms-2" value={item?.id} />
-                    <img src={item?.photo ? `${process.env.API_URL_IMG}/${item?.photo}` : '../images/photo_profile.png'} width='36px' height='36px' className='rounded-circle ms-3' />
+                    <img src={item?.photo ? `${process.env.API_URL_IMG}/${item?.photo}` : '../../images/photo_profile.png'} width='36px' height='36px' className='rounded-circle ms-3' />
                     <p className='w-100 mx-3 pt-3'>{item?.username}</p>
-                    <p className='fw-bold text-center mt-3'><img src='../icon/list-icon.svg' /></p>
+                    <p className='fw-bold text-center mt-3'><img src='../../icon/list-icon.svg' /></p>
                   </div>
                 </>)
               })}
