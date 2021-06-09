6a08da4e602940ab8eed839c5c9bfaad0926fde8
deaedria <dea.edria@gmail.com>
2021-06-09 20:42:53 +0700

    dashboard page (user)

--- pages/[day].js
+++ pages/[day].js
@@ -11,17 +11,30 @@ import { useScheduleById } from './api/schedule/useScheduleById'
 
 function Home() {
   const router = useRouter()
+  const { day, date } = router.query
+  const { day: myDay, ondate, by } = router.query
+
+  let id = null
   const { data: auth } = useSWR('api/users/getSession')
+  const { list: data, mutateList: mutate } = useScheduleByDay(day?.charAt(0).toUpperCase() + day?.slice(1), id, auth?.user?.token)
+  const { list: myData, mutateList: mutateList } = useScheduleById(myDay?.charAt(0).toUpperCase() + myDay?.slice(1), auth?.user?.user_id, auth?.user?.token)
 
   const [index, setIndex] = useState(0);
   const [value, setValue] = useState(null);
   const [theDay, setTheDay] = useState(null);
+  const [active, setActive] = useState(1);
   const [activeTabs, setActiveTabs] = useState(1);
 
   const handleSelect = (selectedIndex, e) => {
     setIndex(selectedIndex);
   };
-  console.log(activeTabs)
+
+  useEffect(() => {
+    if (by) {
+      setActiveTabs(2)
+    }
+  }, [by])
+
   const today = moment().format("D MMMM gggg");
   const today1 = moment().format("YYYY-MM-DD");
   const today2 = moment().format("dddd");
@@ -30,23 +43,19 @@ function Home() {
   const startOfWeek = moment(value ? value : setValue(today1)).startOf('isoWeek');
   const endOfWeek = moment(value ? value : setValue(today1)).endOf('isoWeek');
 
-  let id = null
-  const { list: data, mutateList: mutate } = useScheduleByDay(theDay ? theDay : today2, id, auth?.user?.token)
-  const { list: myData, mutateList: mutateData } = useScheduleById(theDay ? theDay : today2, auth?.user?.user_id, auth?.user?.token)
-
   var days = [];
-  var day = startOfWeek;
+  var day1 = startOfWeek;
 
-  while (day <= endOfWeek) {
-    days.push(day.toDate());
-    day = day.clone().add(1, 'd');
+  while (day1 <= endOfWeek) {
+    days.push(day1.toDate());
+    day1 = day1.clone().add(1, 'd');
   }
 
   var week = days.map(function (dayOfWeek, i) {
     return moment(dayOfWeek).format("dd")
   });
 
-  var date = days.map(function (dayOfWeek, i) {
+  var date1 = days.map(function (dayOfWeek, i) {
     return moment(dayOfWeek).format("DD")
   });
 
@@ -108,42 +117,42 @@ function Home() {
                   <Col xs={12} className='mt-3 w-100' style={{ height: 'min-content', borderTopLeftRadius: '30px', borderTopRightRadius: '30px' }}>
                     <h5 className="py-3 bg-white px-4 mb-0 fw-500" style={{ borderTopLeftRadius: '30px', borderTopRightRadius: '30px' }}>
                       Schedule List
-                      <input min={fullDate[0]} max={fullDate[6]} type="date" className="float-end" id="dateInput" name="dateInput" onChange={(e) => { setValue(e.target.value); activeTabs === 1 ? router.push(`/${moment(e.target.value).format("dddd")}?date=${moment(e.target.value).format("DD")}`) : router.push(`/${moment(e.target.value).format("dddd")}?ondate=${moment(e.target.value).format("DD")}&by=${auth?.user?.user_id}`) }}></input>
+                      <input min={fullDate[0]} max={fullDate[6]} type="date" className="float-end" id="dateInput" name="dateInput" onChange={(e) => { setValue(e.target.value); activeTabs === 1 ? router.push(`/${moment(e.target.value).format("dddd")}?date=${moment(e.target.value).format("DD")}`) : router.push(`/${moment(e.target.value).format("dddd")}?ondate=${moment(e.target.value).format("DD")}&by=${auth?.user?.user_id}`); window.location.href = by ? `/${moment(e.target.value).format("dddd")}?ondate=${moment(e.target.value).format("DD")}&by=${auth?.user?.user_id}` : `/${moment(e.target.value).format("dddd")}?date=${moment(e.target.value).format("DD")}` }}></input>
                     </h5>
                     <div className="d-flex bg-white d-flex align-items-center justify-content-center">
-                      <Button className={`${pickDay == date[0] ? 'btn-blue-dark' : 'bg-white text-black'} b-hover border-radius-10 shadow-none border-0 me-4`} onClick={(e) => { setValue(fullDate[0]); setTheDay(byDay[0]); activeTabs === 1 ? router.push(`/${byDay[0].toLowerCase()}?date=${date[0]}`) : router.push(`/${byDay[0].toLowerCase()}?ondate=${date[0]}&by=${auth?.user?.user_id}`)  }}>
+                      <Button className={`${date == date1[0] || ondate == date1[0] ? 'btn-blue-dark' : 'bg-white text-black'} b-hover border-radius-10 shadow-none border-0 me-4`} onClick={(e) => { setValue(fullDate[0]); setTheDay(byDay[0]); activeTabs === 1 ? router.push(`/${byDay[0].toLowerCase()}?date=${date1[0]}`) : router.push(`/${byDay[0].toLowerCase()}?ondate=${date1[0]}&by=${auth?.user?.user_id}`); window.location.href = by || activeTabs === 2 ? `/${byDay[0].toLowerCase()}?ondate=${date1[0]}&by=${auth?.user?.user_id}` : `/${byDay[0].toLowerCase()}?date=${date1[0]}`; by ? setActiveTabs(2) : setActiveTabs(1) }}>
                         <p className="fw-600">{week[0]} </p>
-                        <p className="fw-600">{date[0]} </p>
+                        <p className="fw-600">{date1[0]} </p>
                       </Button>
-                      <Button className={`${pickDay == date[1] ? 'btn-blue-dark' : 'bg-white text-black'} b-hover border-radius-10 shadow-none border-0 me-4`} onClick={(e) => { setValue(fullDate[1]); setTheDay(byDay[1]); activeTabs === 1 ? router.push(`/${byDay[1].toLowerCase()}?date=${date[1]}`) : router.push(`/${byDay[1].toLowerCase()}?ondate=${date[1]}&by=${auth?.user?.user_id}`)  }}>
+                      <Button className={`${date == date1[1] || ondate == date1[1] ? 'btn-blue-dark' : 'bg-white text-black'} b-hover border-radius-10 shadow-none border-0 me-4`} onClick={(e) => { setValue(fullDate[1]); setTheDay(byDay[1]); activeTabs === 1 ? router.push(`/${byDay[1].toLowerCase()}?date=${date1[1]}`) : router.push(`/${byDay[1].toLowerCase()}?ondate=${date1[1]}&by=${auth?.user?.user_id}`); window.location.href = by || activeTabs === 2 ? `/${byDay[1].toLowerCase()}?ondate=${date1[1]}&by=${auth?.user?.user_id}` : `/${byDay[1].toLowerCase()}?date=${date1[1]}`; by ? setActiveTabs(2) : setActiveTabs(1) }}>
                         <p className="fw-600">{week[1]} </p>
-                        <p className="fw-600">{date[1]} </p>
+                        <p className="fw-600">{date1[1]} </p>
                       </Button>
-                      <Button className={`${pickDay == date[2] ? 'btn-blue-dark' : 'bg-white text-black'} b-hover border-radius-10 shadow-none border-0 me-4`} onClick={(e) => { setValue(fullDate[2]); setTheDay(byDay[2]); activeTabs === 1 ? router.push(`/${byDay[2].toLowerCase()}?date=${date[2]}`) : router.push(`/${byDay[2].toLowerCase()}?ondate=${date[2]}&by=${auth?.user?.user_id}`)  }}>
+                      <Button className={`${date == date1[2] || ondate == date1[2] ? 'btn-blue-dark' : 'bg-white text-black'} b-hover border-radius-10 shadow-none border-0 me-4`} onClick={(e) => { setValue(fullDate[2]); setTheDay(byDay[2]); activeTabs === 1 ? router.push(`/${byDay[2].toLowerCase()}?date=${date1[2]}`) : router.push(`/${byDay[2].toLowerCase()}?ondate=${date1[2]}&by=${auth?.user?.user_id}`); window.location.href = by || activeTabs === 2 ? `/${byDay[2].toLowerCase()}?ondate=${date1[2]}&by=${auth?.user?.user_id}` : `/${byDay[2].toLowerCase()}?date=${date1[2]}`; by ? setActiveTabs(2) : setActiveTabs(1) }}>
                         <p className="fw-600">{week[2]} </p>
-                        <p className="fw-600">{date[2]} </p>
+                        <p className="fw-600">{date1[2]} </p>
                       </Button>
-                      <Button className={`${pickDay == date[3] ? 'btn-blue-dark' : 'bg-white text-black'} b-hover border-radius-10 shadow-none border-0 me-4`} onClick={(e) => { setValue(fullDate[3]); setTheDay(byDay[3]); activeTabs === 1 ? router.push(`/${byDay[3].toLowerCase()}?date=${date[3]}`) : router.push(`/${byDay[3].toLowerCase()}?ondate=${date[3]}&by=${auth?.user?.user_id}`)  }}>
+                      <Button className={`${date == date1[3] || ondate == date1[3] ? 'btn-blue-dark' : 'bg-white text-black'} b-hover border-radius-10 shadow-none border-0 me-4`} onClick={(e) => { setValue(fullDate[3]); setTheDay(byDay[3]); activeTabs === 1 ? router.push(`/${byDay[3].toLowerCase()}?date=${date1[3]}`) : router.push(`/${byDay[3].toLowerCase()}?ondate=${date1[3]}&by=${auth?.user?.user_id}`); window.location.href = by || activeTabs === 2 ? `/${byDay[3].toLowerCase()}?ondate=${date1[3]}&by=${auth?.user?.user_id}` : `/${byDay[3].toLowerCase()}?date=${date1[3]}`; by ? setActiveTabs(2) : setActiveTabs(1) }}>
                         <p className="fw-600">{week[3]} </p>
-                        <p className="fw-600">{date[3]} </p>
+                        <p className="fw-600">{date1[3]} </p>
                       </Button>
-                      <Button className={`${pickDay == date[4] ? 'btn-blue-dark' : 'bg-white text-black'} b-hover border-radius-10 shadow-none border-0 me-4`} onClick={(e) => { setValue(fullDate[4]); setTheDay(byDay[4]); activeTabs === 1 ? router.push(`/${byDay[4].toLowerCase()}?date=${date[4]}`) : router.push(`/${byDay[4].toLowerCase()}?ondate=${date[4]}&by=${auth?.user?.user_id}`)  }}>
+                      <Button className={`${date == date1[4] || ondate == date1[4] ? 'btn-blue-dark' : 'bg-white text-black'} b-hover border-radius-10 shadow-none border-0 me-4`} onClick={(e) => { setValue(fullDate[4]); setTheDay(byDay[4]); activeTabs === 1 ? router.push(`/${byDay[4].toLowerCase()}?date=${date1[4]}`) : router.push(`/${byDay[4].toLowerCase()}?ondate=${date1[4]}&by=${auth?.user?.user_id}`); window.location.href = by || activeTabs === 2 ? `/${byDay[4].toLowerCase()}?ondate=${date1[4]}&by=${auth?.user?.user_id}` : `/${byDay[4].toLowerCase()}?date=${date1[4]}`; by ? setActiveTabs(2) : setActiveTabs(1) }}>
                         <p className="fw-600">{week[4]} </p>
-                        <p className="fw-600">{date[4]} </p>
+                        <p className="fw-600">{date1[4]} </p>
                       </Button>
-                      <Button className={`${pickDay == date[5] ? 'btn-blue-dark' : 'bg-white text-black'} b-hover border-radius-10 shadow-none border-0 me-4`} onClick={(e) => { setValue(fullDate[5]); setTheDay(byDay[5]); activeTabs === 1 ? router.push(`/${byDay[5].toLowerCase()}?date=${date[5]}`) : router.push(`/${byDay[5].toLowerCase()}?ondate=${date[5]}&by=${auth?.user?.user_id}`)  }}>
+                      <Button className={`${date == date1[5] || ondate == date1[5] ? 'btn-blue-dark' : 'bg-white text-black'} b-hover border-radius-10 shadow-none border-0 me-4`} onClick={(e) => { setValue(fullDate[5]); setTheDay(byDay[5]); activeTabs === 1 ? router.push(`/${byDay[5].toLowerCase()}?date=${date1[5]}`) : router.push(`/${byDay[5].toLowerCase()}?ondate=${date1[5]}&by=${auth?.user?.user_id}`); window.location.href = by || activeTabs === 2 ? `/${byDay[5].toLowerCase()}?ondate=${date1[5]}&by=${auth?.user?.user_id}` : `/${byDay[5].toLowerCase()}?date=${date1[5]}`; by ? setActiveTabs(2) : setActiveTabs(1) }}>
                         <p className="fw-600">{week[5]} </p>
-                        <p className="fw-600">{date[5]} </p>
+                        <p className="fw-600">{date1[5]} </p>
                       </Button>
-                      <Button className={`${pickDay == date[6] ? 'btn-blue-dark' : 'bg-white text-black'} b-hover border-radius-10 shadow-none border-0 me-3`} onClick={(e) => { setValue(fullDate[6]); setTheDay(byDay[6]); activeTabs === 1 ? router.push(`/${byDay[6].toLowerCase()}?date=${date[6]}`) : router.push(`/${byDay[6].toLowerCase()}?ondate=${date[6]}&by=${auth?.user?.user_id}`)  }}>
+                      <Button className={`${date == date1[6] || ondate == date1[6] ? 'btn-blue-dark' : 'bg-white text-black'} b-hover border-radius-10 shadow-none border-0 me-3`} onClick={(e) => { setValue(fullDate[6]); setTheDay(byDay[6]); activeTabs === 1 ? router.push(`/${byDay[6].toLowerCase()}?date=${date1[6]}`) : router.push(`/${byDay[6].toLowerCase()}?ondate=${date1[6]}&by=${auth?.user?.user_id}`); window.location.href = by || activeTabs === 2 ? `/${byDay[6].toLowerCase()}?ondate=${date1[6]}&by=${auth?.user?.user_id}` : `/${byDay[6].toLowerCase()}?date=${date1[6]}`; by ? setActiveTabs(2) : setActiveTabs(1) }}>
                         <p className="fw-600">{week[6]} </p>
-                        <p className="fw-600">{date[6]} </p>
+                        <p className="fw-600">{date1[6]} </p>
                       </Button>
                     </div>
 
                     <div className='d-flex justify-content-center bg-white d-tab'>
-                      <div className={`mx-3 py-1 tab fs-900 ${activeTabs === 1 ? 'text-blue-dark' : 'text-grey-dark'}`} onClick={() => setActiveTabs(1)} >All schedule</div>
-                      <div className={`mx-3 py-1 tab fs-900 ${activeTabs === 2 ? 'text-blue-dark' : 'text-grey-dark'}`} onClick={() => setActiveTabs(2)} >For you</div>
+                      <div className={`mx-3 py-1 tab ${activeTabs === 1 ? 'text-blue-dark' : 'text-grey-dark'}`} onClick={() => setActiveTabs(1)} >All schedule</div>
+                      <div className={`mx-3 py-1 tab ${activeTabs === 2 ? 'text-blue-dark' : 'text-grey-dark'}`} onClick={() => setActiveTabs(2)} >For you</div>
                     </div>
 
                     <div className={`py-2 tab ${activeTabs === 1 ? 'd-block bg-white' : 'd-none'}`}>
